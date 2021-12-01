2da92bb65b61a4fb7da468665d79a6d537e98074
istianahretna <80499052+istianahretna@users.noreply.github.com>
2021-12-01 18:54:25 +0700

    Add files via upload

---
+++ cari_mhs.php
@@ -0,0 +1,45 @@
+<?php
+    //melakukan koneksi dengan database melalui file koneksi.php
+    include 'koneksi.php';
+?>
+
+<!-- Judul pada halaman web -->
+<h3>Form Pencarian Dengan PHP MAHASISWA</h3>
+
+<form action="" method="get">
+    <label>Cari :</label>
+    <input type="text" name="cari"> 
+    <input type="submit" value="Cari">
+</form>
+
+<?php
+    if(isset($_GET['cari'])){
+    $cari = $_GET['cari'];
+    echo "<b>Hasil pencarian : ".$cari."</b>";
+    }
+?>
+
+<table border="1">
+<tr>
+    <th>No</th>
+    <th>Nama</th>
+</tr>
+
+<?php if(isset($_GET['cari'])){
+    $cari = $_GET['cari'];
+    $sql="select * from mahasiswa where nama like'%".$cari."%'";
+    $tampil = mysqli_query($con,$sql);
+    }else{
+    $sql="select * from mahasiswa";
+    $tampil = mysqli_query($con,$sql);
+    }
+    $no = 1;
+    while($r = mysqli_fetch_array($tampil)){
+?>
+
+<tr>
+<td><?php echo $no++; ?></td>
+<td><?php echo $r['Nama']; ?></td>
+</tr>
+<?php } ?>
+</table>
